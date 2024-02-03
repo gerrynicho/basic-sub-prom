13d84892807197fef7d833933feccd311a874851
gerrynicho <gerrynicholas76@gmail.com>
2024-02-04 01:45:34 +0700

    connected node to prom (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -28,11 +28,22 @@ private:
     void topic_callback(const tachimawari_interfaces::msg::Joint & msg) const
     {
         RCLCPP_INFO_STREAM(this->get_logger(), "Report " << msg.id << ":" << msg.position);
+        joint_one.Set(msg.position);
     }
     rclcpp::Subscription<tachimawari_interfaces::msg::Joint>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[]){
+    Exposer exposer{"127.0.0.1:7500"}; //localhost
+    auto registry = std::make_shared<Registry>();
+    auto& Joint = BuildGauge()
+                    .Name("joint_data")
+                    .Help("Joints that will be monitored")
+                    .Register(*registry);
+    auto& joint_one = Joint.Add({{"id", "one"}});
+    auto& joint_two = Joint.Add({{"id", "two"}});
+    exposer.RegisterCollectable(registry);
+
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<JointSubscriber>());
     rclcpp::shutdown();
